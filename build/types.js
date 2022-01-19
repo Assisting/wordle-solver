6d3adcd4e61af271e1b37da3d42577de00558f31
Erik LaBine <erik@eriklabine.com>
2022-01-18 22:45:14 -0600

    Delete build directory
    I should really get that in .gitignore

--- build/types.js
+++
@@ -1,9 +0,0 @@
-export class LetterAndPlace {
-    Place;
-    Letter;
-    constructor(letter, place) {
-        this.Letter = letter;
-        this.Place = place;
-    }
-}
-//# sourceMappingURL=types.js.map
